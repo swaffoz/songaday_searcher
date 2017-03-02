5727073f819360fab6d6f7e71491331da4989af0
Zane Swafford <zane@otters.io>
2017-03-01 21:15:21 -0600

    Added initial model tests

--- songs/models.py
+++ songs/models.py
@@ -1,20 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Tag(models.Model):
-    text = models.CharField(max_length=255)
+    text = models.CharField(max_length=255, unique=True, validators=[MinLengthValidator(2)])
 
     def __str__(self):
         return self.text
 
 
 class Song(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    url = models.CharField(max_length=255)
-    view_count = models.IntegerField(null=True)
-    like_count = models.IntegerField(null=True)
-    dislike_count = models.IntegerField(null=True)
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(2)])
+    description = models.TextField(blank=True, null=True)
+    url = models.CharField(max_length=255, validators=[MinLengthValidator(4)])
+    view_count = models.IntegerField(null=True, blank=True)
+    like_count = models.IntegerField(null=True, blank=True)
+    dislike_count = models.IntegerField(null=True, blank=True)
     song_number = models.IntegerField()
     thumbnail_url = models.CharField(blank=True, null=True, max_length=255)
     release_date = models.DateField()
---
+++ songs/tests/test_models.py
@@ -0,0 +1,129 @@
+import datetime
+
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+
+from songs.models import Song, Tag
+
+
+class SongModelTestCase(TestCase):
+
+    def test_empty_songs_cannot_save(self):
+        """
+        Songs must have the song_number and release_date fields filled in.
+        """
+        with self.assertRaises(IntegrityError):
+            Song.objects.create()
+
+    def test_song_with_short_title_is_invalid(self):
+        """
+        Songs should have a title of 2 characters or more. Any less is invalid.
+        """
+        song_number = 0
+        title = 'a'
+        url = 'https://zaneswafford.com/'
+        release_date = datetime.date.today()
+
+        song = Song.objects.create(song_number=song_number,
+                                   title=title,
+                                   url=url,
+                                   release_date=release_date)
+
+        with self.assertRaises(ValidationError):
+            song.clean_fields()
+
+
+    def test_song_with_short_url_is_invalid(self):
+        """
+        Songs should have a url of 4 characters or more. Any less is invalid.
+        """
+        song_number = 1
+        title = 'My Song'
+        url = 'htt'
+        release_date = datetime.date.today()
+
+        song = Song.objects.create(song_number=song_number,
+                                   title=title,
+                                   url=url,
+                                   release_date=release_date)
+
+        with self.assertRaises(ValidationError):
+            song.clean_fields()
+
+    def test_valid_song_does_save(self):
+        """
+        Saving a Song with appropriate data should succeed and have clean fields.
+        """
+        song_number = 1
+        title = 'My Song'
+        description = 'This is my song'
+        url = 'https://zaneswafford.com/'
+        release_date = datetime.date.today()
+        view_count = 987
+        like_count = 654
+        dislike_count = 321
+        thumbnail_url = 'https://otters.io/'
+        tag = Tag.objects.create(text='mytag')
+
+        song = Song.objects.create(song_number = song_number,
+                                   title = title,
+                                   description = description,
+                                   url = url,
+                                   release_date = release_date,
+                                   view_count = view_count,
+                                   like_count = like_count,
+                                   dislike_count = dislike_count,
+                                   thumbnail_url = thumbnail_url)
+        song.tags.add(tag)
+        song.save()
+        song.clean_fields()
+
+    def test_song_string_representation(self):
+        """
+        A Song should show its song number and title in its string representation.
+        """
+        song_number = 1
+        title = 'My Song'
+        url = 'https://zaneswafford.com/'
+        release_date = datetime.date.today()
+
+        song = Song.objects.create(song_number = song_number,
+                                   title = title,
+                                   url = url,
+                                   release_date = release_date)
+
+        self.assertIn(str(song.song_number), str(song))
+        self.assertIn(str(song.title), str(song))
+
+
+class TagModelTestCase(TestCase):
+
+    def test_tag_with_short_text_is_invalid(self):
+        """
+        Tags should have a text of 2 characters or more. Any less is invalid.
+        """
+        text = "T"
+        tag = Tag.objects.create(text=text)
+
+        with self.assertRaises(ValidationError):
+            tag.clean_fields()
+
+    def test_tags_must_be_unique(self):
+        """
+        Tag objects must have unique text. 
+        Two tags cannot be the same.
+        """
+        with self.assertRaises(IntegrityError):
+            text = "mytag"
+            Tag.objects.create(text=text)
+            Tag.objects.create(text=text)
+
+    def test_tag_string_representation(self):
+        """
+        A Tag should show its text in its string representation.
+        """
+        text = "mytag"
+        tag = Tag.objects.create(text=text)
+
+        self.assertIn(str(tag.text), str(tag))
