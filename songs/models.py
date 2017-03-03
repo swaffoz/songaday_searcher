d129d8e9e2fc906be9bdbbab0b7e9e8f31a2cc86
Zane Swafford <zane@otters.io>
2017-03-03 00:40:08 -0600

    Added updated models

--- songs/models.py
+++ songs/models.py
@@ -2,15 +2,31 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
+class TagManager(models.Manager):
+
+    def get_by_natural_key(self, id, text):
+        return self.get(id=id)
+
+
 class Tag(models.Model):
-    text = models.CharField(max_length=255, unique=True, validators=[MinLengthValidator(2)])
+    objects = TagManager()
+
+    text = models.CharField(max_length=255, unique=True,
+                            validators=[MinLengthValidator(2)])
+
+    # Natural key has to be a tuple for deserialization to work.
+    # This is the most logical tuple I could come up with. I wish we could use
+    # only text.
+    def natural_key(self):
+        return (self.id, self.text)
 
     def __str__(self):
         return self.text
 
 
 class Song(models.Model):
-    title = models.CharField(max_length=255, validators=[MinLengthValidator(2)])
+    title = models.CharField(max_length=255, validators=[
+                             MinLengthValidator(2)])
     description = models.TextField(blank=True, null=True)
     url = models.CharField(max_length=255, validators=[MinLengthValidator(4)])
     view_count = models.IntegerField(null=True, blank=True)
