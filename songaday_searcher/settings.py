1e6cdc06e0f9605ac288abd00d3c3243eb3d1c36
Zane Swafford <zane@otters.io>
2017-03-14 23:14:14 -0500

    Added YOUTUBE API setting to settings and celery settings

--- songaday_searcher/settings.py
+++ songaday_searcher/settings.py
@@ -123,3 +123,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Celery Settings
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+
+# Youtube API Key
+
+# 'AIzaSyCDLipz8lXoJkq5APH9820wWPkCLwNtqhk'
+YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
