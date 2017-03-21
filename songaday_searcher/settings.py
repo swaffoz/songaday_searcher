7a44a843f112b9391cff917f9e92fed479f5b44c
Zane Swafford <zane@otters.io>
2017-03-21 13:14:13 -0500

    Changed celery timezone to match django's timezone

--- songaday_searcher/settings.py
+++ songaday_searcher/settings.py
@@ -153,7 +153,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
+CELERY_TIMEZONE = 'America/New_York'
 
 
 # Youtube API Key
